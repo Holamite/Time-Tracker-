032351916e66bc07cbd3ff316f7b2e24887c5d37
holamite <olamideadebara@gmail.com>
2025-08-25 11:32:58 +0100

    first commit

---
+++ target/debug/deps/track-906d5a539be43772.d
@@ -0,0 +1,5 @@
+/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/track-906d5a539be43772.d: src/lib.rs
+
+/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/libtrack-906d5a539be43772.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/track-e7b8ae500925e1f1.d
@@ -0,0 +1,5 @@
+/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/track-e7b8ae500925e1f1.d: src/lib.rs
+
+/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/libtrack-e7b8ae500925e1f1.rmeta: src/lib.rs
+
+src/lib.rs:
