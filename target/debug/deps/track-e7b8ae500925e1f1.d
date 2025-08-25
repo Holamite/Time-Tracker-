be418c001265a7c3835bb7d0775afa055b3cfe00
holamite <olamideadebara@gmail.com>
2025-08-25 11:36:34 +0100

    Stop tracking target folder

--- target/debug/deps/track-e7b8ae500925e1f1.d
+++
@@ -1,5 +0,0 @@
-/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/track-e7b8ae500925e1f1.d: src/lib.rs
-
-/Users/pc/Desktop/Rust tutorial/ztm-rust/projects/track/target/debug/deps/libtrack-e7b8ae500925e1f1.rmeta: src/lib.rs
-
-src/lib.rs:
